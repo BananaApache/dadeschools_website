6405754436d0888e504d1812659a91b198ff514c
BananaApache <daniel.miami2005@gmail.com>
2023-02-08 22:51:44 -0500

    Made even more responsive

--- dade_website/static/bruh.js
+++ dade_website/static/bruh.js
@@ -296,125 +296,130 @@ function showPanel() {
 
 
 function addNewAssignment(name, category, percent, d) {
-    document.querySelector("#ContentMain > div:nth-child(3)").removeChild(d.parentElement)
-
-    const tbody = document.querySelector("#Assignments > tbody")
-
-    const newAssignment = document.createElement("tr")
-    newAssignment.setAttribute("class", "assignment")
-
-    const _date = document.createElement("td")
-    _date.setAttribute("class", "icon-date")
-    const _time = document.createElement("time")
-    _time.setAttribute("class", "date-due")
-    _time.setAttribute("datetime", "2022-09-11T00:00:00")
-    _date.append(_time)
-    const _span = document.createElement("span")
-    _time.innerHTML = "<span>Sep</span>11 "
-
-    const _description = document.createElement("td")
-    _description.setAttribute("class", "description")
-    const _title = document.createElement("div")
-    _title.setAttribute("class", "title")
-    _description.append(_title)
-    _title.innerHTML = name
-    const _category = document.createElement("div")
-    _category.setAttribute("class", "category")
-    _description.append(_category)
-    _category.innerHTML = category
-    
-    const _numbers = document.createElement("td")
-    _numbers.setAttribute("class", "numeric")
-    const _numeric = document.createElement("div")
-    _numeric.setAttribute("class", "numeric")
-    _numbers.append(_numeric)
-    const _points = document.createElement("span")
-    _points.setAttribute("class", "points")
-    _numeric.append(_points)
-    _points.innerHTML = percent.slice(0, -1)
-    const _max = document.createElement("div")
-    _max.setAttribute("class", "max")
-    _numeric.append(_max)
-    _max.innerHTML = "max 100"
-
-    const _letter = document.createElement("td")
-    _letter.setAttribute("class", "letter")
-    const _l1 = document.createElement("div")
-    _l1.setAttribute("class", "letter")
-
-    const final_grade = percent.slice(0, -1)
-    if (final_grade >= 90) {
-        _l1.setAttribute("style", `color: ${Alettercolor}; background-color: ${Abackgroundcolor}`)
-        _letter.append(_l1)
-        const _l2 = document.createElement("div")
-        _l1.append(_l2)
-        const _l3 = document.createElement("div")
-        _l2.append(_l3)
-        _l3.innerHTML = 'A'
-        const _l4 = document.createElement("span")
-        _l4.setAttribute("class", "percent")
-        _l3.append(_l4)
-        _l4.innerHTML = percent
+    if (category == "Select a Category") {
+        void(0)
     }
-    else if(80 <= final_grade && final_grade <= 89) {
-        _l1.setAttribute("style", `color: ${Blettercolor}; background-color: ${Bbackgroundcolor}`)
-        _letter.append(_l1)
-        const _l2 = document.createElement("div")
-        _l1.append(_l2)
-        const _l3 = document.createElement("div")
-        _l2.append(_l3)
-        _l3.innerHTML = 'B'
-        const _l4 = document.createElement("span")
-        _l4.setAttribute("class", "percent")
-        _l3.append(_l4)
-        _l4.innerHTML = percent
-    }
-    else if(70 <= final_grade && final_grade <= 79) {
-        _l1.setAttribute("style", `color: ${Clettercolor}; background-color: ${Cbackgroundcolor}`)
-        _letter.append(_l1)
-        const _l2 = document.createElement("div")
-        _l1.append(_l2)
-        const _l3 = document.createElement("div")
-        _l2.append(_l3)
-        _l3.innerHTML = 'C'
-        const _l4 = document.createElement("span")
-        _l4.setAttribute("class", "percent")
-        _l3.append(_l4)
-        _l4.innerHTML = percent
-    }
-    else if(60 <= final_grade && final_grade <= 69) {
-        _l1.setAttribute("style", `color: ${Dlettercolor}; background-color: ${Dbackgroundcolor}`)
-        _letter.append(_l1)
-        const _l2 = document.createElement("div")
-        _l1.append(_l2)
-        const _l3 = document.createElement("div")
-        _l2.append(_l3)
-        _l3.innerHTML = 'D'
-        const _l4 = document.createElement("span")
-        _l4.setAttribute("class", "percent")
-        _l3.append(_l4)
-        _l4.innerHTML = percent
-    }
-    else if(0 <= final_grade && final_grade <= 59) {
-        _l1.setAttribute("style", `color: ${Flettercolor}; background-color: ${Fbackgroundcolor}`)
-        _letter.append(_l1)
-        const _l2 = document.createElement("div")
-        _l1.append(_l2)
-        const _l3 = document.createElement("div")
-        _l2.append(_l3)
-        _l3.innerHTML = 'F'
-        const _l4 = document.createElement("span")
-        _l4.setAttribute("class", "percent")
-        _l3.append(_l4)
-        _l4.innerHTML = percent
-    }
+    else {
+        document.querySelector("#ContentMain > div:nth-child(3)").removeChild(d.parentElement)
 
-    newAssignment.append(_date)
-    newAssignment.append(_description)
-    newAssignment.append(_numbers)
-    newAssignment.append(_letter)
+        const tbody = document.querySelector("#Assignments > tbody")
 
-    tbody.prepend(newAssignment)
+        const newAssignment = document.createElement("tr")
+        newAssignment.setAttribute("class", "assignment")
 
-    getFinalGrade(document.querySelector("#root").children[0])
+        const _date = document.createElement("td")
+        _date.setAttribute("class", "icon-date")
+        const _time = document.createElement("time")
+        _time.setAttribute("class", "date-due")
+        _time.setAttribute("datetime", "2022-09-11T00:00:00")
+        _date.append(_time)
+        const _span = document.createElement("span")
+        _time.innerHTML = "<span>Sep</span>11 "
+
+        const _description = document.createElement("td")
+        _description.setAttribute("class", "description")
+        const _title = document.createElement("div")
+        _title.setAttribute("class", "title")
+        _description.append(_title)
+        _title.innerHTML = name
+        const _category = document.createElement("div")
+        _category.setAttribute("class", "category")
+        _description.append(_category)
+        _category.innerHTML = category
+        
+        const _numbers = document.createElement("td")
+        _numbers.setAttribute("class", "numeric")
+        const _numeric = document.createElement("div")
+        _numeric.setAttribute("class", "numeric")
+        _numbers.append(_numeric)
+        const _points = document.createElement("span")
+        _points.setAttribute("class", "points")
+        _numeric.append(_points)
+        _points.innerHTML = percent.slice(0, -1)
+        const _max = document.createElement("div")
+        _max.setAttribute("class", "max")
+        _numeric.append(_max)
+        _max.innerHTML = "max 100"
+
+        const _letter = document.createElement("td")
+        _letter.setAttribute("class", "letter")
+        const _l1 = document.createElement("div")
+        _l1.setAttribute("class", "letter")
+
+        const final_grade = percent.slice(0, -1)
+        if (final_grade >= 90) {
+            _l1.setAttribute("style", `color: ${Alettercolor}; background-color: ${Abackgroundcolor}`)
+            _letter.append(_l1)
+            const _l2 = document.createElement("div")
+            _l1.append(_l2)
+            const _l3 = document.createElement("div")
+            _l2.append(_l3)
+            _l3.innerHTML = 'A'
+            const _l4 = document.createElement("span")
+            _l4.setAttribute("class", "percent")
+            _l3.append(_l4)
+            _l4.innerHTML = percent
+        }
+        else if(80 <= final_grade && final_grade <= 89) {
+            _l1.setAttribute("style", `color: ${Blettercolor}; background-color: ${Bbackgroundcolor}`)
+            _letter.append(_l1)
+            const _l2 = document.createElement("div")
+            _l1.append(_l2)
+            const _l3 = document.createElement("div")
+            _l2.append(_l3)
+            _l3.innerHTML = 'B'
+            const _l4 = document.createElement("span")
+            _l4.setAttribute("class", "percent")
+            _l3.append(_l4)
+            _l4.innerHTML = percent
+        }
+        else if(70 <= final_grade && final_grade <= 79) {
+            _l1.setAttribute("style", `color: ${Clettercolor}; background-color: ${Cbackgroundcolor}`)
+            _letter.append(_l1)
+            const _l2 = document.createElement("div")
+            _l1.append(_l2)
+            const _l3 = document.createElement("div")
+            _l2.append(_l3)
+            _l3.innerHTML = 'C'
+            const _l4 = document.createElement("span")
+            _l4.setAttribute("class", "percent")
+            _l3.append(_l4)
+            _l4.innerHTML = percent
+        }
+        else if(60 <= final_grade && final_grade <= 69) {
+            _l1.setAttribute("style", `color: ${Dlettercolor}; background-color: ${Dbackgroundcolor}`)
+            _letter.append(_l1)
+            const _l2 = document.createElement("div")
+            _l1.append(_l2)
+            const _l3 = document.createElement("div")
+            _l2.append(_l3)
+            _l3.innerHTML = 'D'
+            const _l4 = document.createElement("span")
+            _l4.setAttribute("class", "percent")
+            _l3.append(_l4)
+            _l4.innerHTML = percent
+        }
+        else if(0 <= final_grade && final_grade <= 59) {
+            _l1.setAttribute("style", `color: ${Flettercolor}; background-color: ${Fbackgroundcolor}`)
+            _letter.append(_l1)
+            const _l2 = document.createElement("div")
+            _l1.append(_l2)
+            const _l3 = document.createElement("div")
+            _l2.append(_l3)
+            _l3.innerHTML = 'F'
+            const _l4 = document.createElement("span")
+            _l4.setAttribute("class", "percent")
+            _l3.append(_l4)
+            _l4.innerHTML = percent
+        }
+
+        newAssignment.append(_date)
+        newAssignment.append(_description)
+        newAssignment.append(_numbers)
+        newAssignment.append(_letter)
+
+        tbody.prepend(newAssignment)
+
+        getFinalGrade(document.querySelector("#root").children[0])
+    }
 }
